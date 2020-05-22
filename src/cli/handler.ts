366033d053a02d105b9582228c76102dd91e75e5
aelbore <aelbore.sg@gmail.com>
2020-05-22 16:01:24 +0800

    remove mocha-headless

--- src/cli/handler.ts
+++ src/cli/handler.ts
@@ -4,16 +4,8 @@ import { parseOptions } from './parse-options'
 import { CommandLineOptions } from './options'
 import { cliRun } from './run'
 
-async function browserHandler(opts?: CommandLineOptions) {
-  const browser = await import('aria-mocha-headless')
-  await browser.handler(opts)
-}
-
 export async function handler(opts?: CommandLineOptions) { 
   const options = updateOptions(opts)
   const coverageOptions = parseOptions(options)
-
-  options.browser || options.target
-    ? await browserHandler(options)
-    : await cliRun(coverageOptions)
+  await cliRun(coverageOptions)
 }
